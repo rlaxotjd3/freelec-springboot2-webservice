795e905e6c9e5ab0eaff20f4eb445dced726c848
taeseong <rlaxotjd12@naver.com>
2020-02-15 21:16:30 +0900

    HomeController, Application, HomeControllerTest 설명 추가

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,6 +3,12 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*프로젝트의 메인 클래스
+* @SpringBootApplication으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정함.
+* @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야 함.
+* SpringApplication.run으로 인해 내장 was 를 실행함( 이렇게 되면 항상 서버에 톰캣을 설치할 필요가 없고. 스프링 부트로 만들어진 JAR 파일로 실행하면 됨.
+* 내장 was를 사용하는 이유: 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수가 있기 때문.
+* */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/jojoldu/book/springboot/web/HomeController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HomeController.java
@@ -2,7 +2,9 @@ package com.jojoldu.book.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*@RestController : 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줍니다.
+* @GetMapping : HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줍니다.
+* 이전까지의 요청 : @RequestMapping(method= RequestMethod.GET) */
 @RestController
 public class HomeController {
 
