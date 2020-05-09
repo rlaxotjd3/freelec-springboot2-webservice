49521cf2afa5caf543fc2461872a45bc8dc60b3f
taeseong <rlaxotjd12@naver.com>
2020-05-09 17:42:53 +0900

    index.mustache 내용 추가, IndexController추가

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됩니다.
+ * 앞의 경로는 src/main/resources/templates로 , 뒤의 파일 확장자는 .mustache가 붙는 것입니다.
+ * 즉 여기선 "index"를 반환하므로 src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리하게 됩니다.
+ *
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
