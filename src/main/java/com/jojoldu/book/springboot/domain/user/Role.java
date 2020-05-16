243ee4f0fa0f611305c18e98e3ca4a3480fae61e
taeseong <rlaxotjd12@naver.com>
2020-05-16 21:37:12 +0900

    user 형태 설정, 스프링 시큐리티에서 사용하기 위한 ROLE 설정, repository 설정

---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String Key;
+    private final String title;
+
+    /*스프링 시큐리티에서는 권한 코드에 항상 ROLE_이 앞에 있어야만 합니다. 그래서 코드별 키 값을 ROLE_GUEST, ROLE_USER
+    * 등으로 지정한 것입니다.*/
+}
