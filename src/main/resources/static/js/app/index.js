153b8aa6b7fbdc3acc694e11b0d83de10a6da0b4
taeseong <rlaxotjd12@naver.com>
2020-05-16 18:47:37 +0900

    index.js 수정 (  update를 위해) index.mustache 수정 ( update 화면 이동 )

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,3 +1,62 @@
+var main = {
+    init: function() {
+        var _this = this;
+        $('#btn-update').on('click', function() { // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update
+            //function을 실행하도록 이벤트를 등록합니다.
+            _this.update();
+        });
+    },
+    save : function() {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다');
+            window.location.href='/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update: function() {
+        var data = {
+            title : $('#title').val(),
+            content: $('#content').val()
+        };
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            /*
+            * 여러 HTTP Method 중 PUT 메소드를 선택합니다.
+            * PostsApiController에 있는 API에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 합니다. 참고로 이는 REST
+            * 규약에 맞게 설정된 것입니다.
+            * REST에서 CRUD는 다음과 같이 HTTP Method에 매핑됩니다.
+            * 생성(Create) - POST
+            * 읽기(Read) - GET
+            * 수정(Update) - PUT
+            * 삭제(Delete) - DELETE*/
+            url: '/api/v1/posts/'+id,
+            //어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가합니다.
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+    }
+};
 var index= {
     init: function() {
         var _this = this;
@@ -28,6 +87,7 @@ var index= {
 
 };
 index.init();
+main.init();
 //index.js의 첫 문장에 var index={}라는 코드를 선언했습니다. 굳이 index라는 변수의 속성으로 function을 추가한 이유는 뭘까요? 
 //브라우저의 스코프는 공용 공간으로 쓰이기 때문에 나중에 로딩된 js의 init, save가 있을 시 먼저 로딩된 js의 scope를 덮어쓰게 됩니다.
 /*
